05b4f0a1062c0ff0b95b69ac8c3baeebcebfefeb
omid beigi <o.beygi@gmail.com>
2023-05-04 11:52:05 +0330

    add empty state component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset/>
+      </ClientOnly>
+    );
+  }
+
   return (
-   <div className="text-rose-500 text-2xl">hello airbnb</div>
-  )
+    <ClientOnly>
+      <Container>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
